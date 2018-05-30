9a4ac951b0426c6a94af175bac7ead6ece343bb3
Giordano Arman <giordanoarman@gmail.com>
2018-05-30 17:17:25 +0200

    Refactored StringCalculator.

--- src/string-calculator/StringCalculator.js
+++ src/string-calculator/StringCalculator.js
@@ -1,20 +1,10 @@
-const _ = require(`lodash`);
-
-const MAX = (Number.MAX_SAFE_INTEGER - 1) / 2;
-const MIN = (Number.MIN_SAFE_INTEGER + 1) / 2;
-
 class StringCalculator {
 
-  static getRandom() {
-    return _.random(MIN, MAX);
-  }
-  
   add(numbers) {
     if (numbers.length <= 0)
       return 0;
-    const value = 
-      numbers.split(`,`).map(v => parseFloat(v)).reduce((a, b) => a + b, 0);
-    return parseInt(value, 10);
+    return parseInt(
+      numbers.split(`,`).map(v => parseFloat(v)).reduce((a, b) => a + b, 0));
   }
 
 }
