daeaf281b3a2aae24d99b35d5e57521a8e8b5609
Giordano Arman <giordanoarman@gmail.com>
2018-05-30 19:22:15 +0200

    Added step 4 logic.

--- src/string-calculator/StringCalculator.js
+++ src/string-calculator/StringCalculator.js
@@ -9,8 +9,25 @@ class StringCalculator {
       this.parse(numbers).map(v => parseFloat(v)).reduce((a, b) => a + b, 0));
   }
 
-  parse(list) {
-    return _.flattenDeep(list.split(`,`).map(v => v.split(`\n`)));
+  parse(numbers) {
+    let value = _.flattenDeep(numbers.split(`,`).map(v => v.split(`\n`)));
+    const delimiter = this.getDelimiter(numbers);
+    if (delimiter) {
+      value = value.map(v => v.split(delimiter)).slice(1);
+    }
+    value = _.flattenDeep(value);
+    // console.log(`value`, value);
+    return value;
+  }
+
+  getDelimiter(numbers) {
+    if ((numbers[0] === `/`) && (numbers[1] === `/`)) {
+      const end = numbers.indexOf(`\n`);
+      if (end > 2) {
+        return numbers.substring(2, end);
+      }
+    }
+    return null;
   }
 
 }
---
+++ src/tests/specs/step4.spec.js
@@ -0,0 +1,27 @@
+const { StringCalculator } = rootRequire(`string-calculator`);
+
+describe(`StringCalculator.prototype.add`, function() {
+ 
+  it(
+    `should allow specifying different delimeters`,
+    async function() {
+      const values = [
+        [`//F\n0F12F23F444F333F2F4F5F6`, 829],
+        [`//;\n0;12;-23;444;333;2;4;5;6`, 783],
+        [`//(\n0(12(-23(444(333(2(4(5(6(12(12(3(2.2`, 812],
+        [`//.\n10.10`, 20],
+        [`//h\n10h20h30`, 60]
+      ];
+
+      const calc = new StringCalculator();
+
+      const matches = values.map(([numbers, sum]) => {
+        // console.log(`calc.add(numbers)`, calc.add(numbers));
+        return calc.add(numbers) === sum;
+      });
+
+      expect(matches.indexOf(false)).toBe(-1);
+    }
+  );
+  
+});
